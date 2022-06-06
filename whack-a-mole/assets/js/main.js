f05dfeeef07d855195f328de91cb8f09c8218260
dibss <ludovicad01@gmail.com>
2022-06-06 22:29:14 +0200

    remove ant and splat img

--- whack-a-mole/assets/js/main.js
+++ whack-a-mole/assets/js/main.js
@@ -1,16 +1,48 @@
+const timeSpan = document.querySelector('#timer');
 let time = 0;
-let score = 0;
+timeSpan.innerText = time;
 
-const timeSpan = document.querySelector('#timer').innerHTML = time;
-const scoreSpan = document.querySelector('#score').innerHTML = score;
+const scoreSpan = document.querySelector('#score');
+let score = 0;
+scoreSpan.innerText = score;
 
 const cells = document.querySelectorAll('.cell');
 
-let randNum = Math.floor(Math.random() * cells.length);
-const cell = cells[randNum];
+const speed = 800;
 
-cell.classList.add('ant');
-
-if (condition) {
-  cell.classList.add('splat');
+function randomAnt(){
+  removeAnt();
+  const randNum = Math.floor(Math.random() * cells.length);
+  const cell = cells[randNum];
+  cell.classList.add('ant');
 }
+
+function removeAnt(){
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    cell.classList.remove('ant')
+  }
+}
+
+const movement = setInterval(randomAnt, speed);
+
+for (let i = 0; i < cells.length; i++) {
+  const cell = cells[i];
+  cell.addEventListener('click', function(){
+    if(cell.classList.contains('ant')) {
+      cell.classList.add('splat');
+      score++;
+      scoreSpan.innerText = score;
+      setTimeout(removeSplat, 300);
+    }
+  })
+}
+
+function removeSplat(){
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    cell.classList.remove('splat')
+  }
+}
+
+
